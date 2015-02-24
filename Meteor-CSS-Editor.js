95bdbfebb64d6710948b81f413c7f81d067e1005
davluzius <davluzius@gmail.com>
2015-02-23 20:43:03 -0800

    add Template editor

--- Meteor-CSS-Editor.js
+++ Meteor-CSS-Editor.js
@@ -1,30 +1,104 @@
-
-Styles = new Meteor.Collection('Styles');
+HTMLCollection = new Meteor.Collection('HTMLCollection');
+StylesCollection = new Meteor.Collection('StylesCollection');
+PeopleCollection = new Meteor.Collection('PeopleCollection');
 
 if (Meteor.isClient){
 
-  Meteor.subscribe('Styles');
+var userId = Random.id();
+var lastHtmlEditorId;
+var lastCssEditorId;
+var cssEditor;
+var htmlEditor;
+var peopleData;
+var parent;
 
-  // get the styles only once, doesn't need to be reactive in this case
-  Styles.find().observeChanges({
+Meteor.startup(function(){
+
+  // fake a user id
+  Meteor.call('setDefaults',userId);
+
+  // data we're going to provide to the template
+  peopleData = PeopleCollection.find();
+
+  // parent Dom element the Template is rendered into
+  parent = document.getElementById('htmlOutput');
+
+});
+
+  StylesCollection.find().observeChanges({
     added: function(id, doc) {
-      document.getElementById('cssEditor').innerHTML = doc.css;
-      applyCSS(doc.css);
+      var textArea = document.getElementById('cmCss');
+      cssEditor = CodeMirror.fromTextArea(textArea);
+      cssEditor.getDoc().setValue(doc.css);
+      cssEditor.getDoc().on("change",saveCSS);
+      renderCSS(doc.css);
+    },
+    changed: function(id,doc){
+
+      if(doc.css){
+        renderCSS(doc.css);
+      }
+      
+      if(doc.lastModifiedBy){
+        lastCssEditorId = doc.lastModifiedBy;
+      }
+
+      if(lastCssEditorId!==userId){
+        console.log("Someone else made an edit to the css!");
+        cssEditor.getDoc().off("change",saveHTML);  // turn off auto save temporarily
+        cssEditor.getDoc().setValue(doc.css);
+        cssEditor.getDoc().on("change",saveHTML);
+      }
     }
   });
+
+  HTMLCollection.find().observeChanges({
+    added: function(id, doc) {
+      var html = getRawHtml(doc.html);
+      var textArea = document.getElementById('cmHtml');
+      htmlEditor = CodeMirror.fromTextArea(textArea);
+      htmlEditor.getDoc().setValue(html);
+      htmlEditor.getDoc().on("change",saveHTML);
+      renderHTML(html,peopleData,parent);
+    },
+    changed: function(id,doc){
+
+      if(doc.html){
+        var html = getRawHtml(doc.html);
+        renderHTML(html,peopleData,parent); 
+      }
+
+      if(doc.lastModifiedBy){
+        lastHtmlEditorId = doc.lastModifiedBy;  // only update my editor if someone else made the change
+      }
+
+      if(lastHtmlEditorId!==userId){
+        console.log("Someone else made an edit to the html!");
+        htmlEditor.getDoc().off("change",saveHTML);  // turn off auto save temporarily
+        htmlEditor.getDoc().setValue(html);
+        htmlEditor.getDoc().on("change",saveHTML);
+      }
+    }
+  });
+
 
   Template.MainTemplate.events({
-    'input #cssEditor' : function(e){
-      var css = e.target.innerHTML;
-      applyCSS(css);
-      var id = Styles.findOne({name:'myStyle'})._id;
-      Styles.update(id,{$set:{css:css}});
+    'click #restoreDefaults' : function(){
+        Meteor.call('restoreDefaults',userId);
     }
-  });
+  })
+
+
+  function saveCSS(codeMirror){
+    newCSS = codeMirror.getValue();
+    var id = StylesCollection.findOne({name:'myStyle'})._id;
+    StylesCollection.update(id,{$set:{css:newCSS,lastModifiedBy:userId}});
+  }
 
   // apply the css string to the CSSOM
-  // this is from : http://stackoverflow.com/questions/524696/how-to-create-a-style-tag-with-javascript
-  function applyCSS(newCSS){
+  // http://stackoverflow.com/questions/524696/how-to-create-a-style-tag-with-javascript
+  function renderCSS(newCSS){
+
     newCSS = newCSS.split('<br>').join("");
     var head = document.head || document.getElementsByTagName('head')[0];
     var style = document.createElement('style');
@@ -37,15 +111,88 @@ if (Meteor.isClient){
     head.appendChild(style);
   }
 
+  function saveHTML(codeMirror){
+
+    newHTML = codeMirror.getValue();
+    var id = HTMLCollection.findOne({name:'myHtml'})._id;
+    HTMLCollection.update(id,{$set:{html:newHTML,lastModifiedBy:userId}});
+  }
+
+  // renders html with a data context into the parent Dom object
+  // you can use spacebars {{ }} template tags in newHTML
+  
+  function renderHTML(newHTML,dataContext,parent){
+
+    clearError();
+    // going to 'try' it all, because we're saving on each edit so
+    // the malformed Blaze Template syntax will throw a lot of errors
+    try{
+      // create the 'HTMLjs' which is used internally by the template
+      // https://meteorhacks.com/how-blaze-works.html
+      var htmlJS = SpacebarsCompiler.compile(newHTML);
+      var evaled = eval(htmlJS);
+      var view = Blaze.With(dataContext,evaled);
+
+      // clear the output and re-render it
+      parent.innerHTML = "";
+      Blaze.render(view,parent);
+      
+    }catch(e){ 
+      displaySyntaxError(e); 
+    }
+  }
+
+  // some helper functions 
+  function displaySyntaxError(e){
+    var errorPanel = document.getElementById('errorPanel');
+    errorPanel.innerHTML = ''+e;
+  }
+  
+  function clearError(e){
+    var errorPanel = document.getElementById('errorPanel');
+    errorPanel.innerHTML = '';
+  }
+
+  function getRawHtml(_html){
+    var txt = document.createElement("textarea");
+    txt.innerHTML = _html;
+    return txt.value;
+  }
+
 }
 
 if (Meteor.isServer){
-  // add a style if none exists yet
-  Meteor.startup(function () {
-    var myCSS = "body{background-color:#ff9900;}"
-    if(!Styles.findOne({name:'myStyle'})){
-      Styles.insert({name:'myStyle',css:myCSS});
+
+  Meteor.methods({
+
+    setDefaults : function(userId){
+    
+      if(!StylesCollection.findOne({name:'myStyle'})){
+        StylesCollection.insert({name:'myStyle',css:MockCSS,lastModifiedBy:userId});
+      }
+      
+      if(!HTMLCollection.findOne({name:'myHtml'})){
+        HTMLCollection.insert({name:'myHtml',html:MockHTML,lastModifiedBy:userId});
+      }
+
+      if(!PeopleCollection.findOne()){
+        for(var i=0;i<MockPeople.length;i++){
+          PeopleCollection.insert(MockPeople[i]);
+        }
+      }
+    },
+
+    restoreDefaults : function(userId){
+
+      HTMLCollection.update({name:'myHtml'},{$set:{html:MockHTML,lastModifiedBy:userId}});
+      
+      StylesCollection.update({name:'myStyle'},{$set:{css:MockCSS,lastModifiedBy:userId}});
+      
+      PeopleCollection.remove({});
+      for(var i=0;i<MockPeople.length;i++){
+        PeopleCollection.insert(MockPeople[i]);
+      }
     }
-  });
+  })
 
 }
