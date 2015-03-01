1e35a0ab8754d4973a92d20ad2f9a2d5ee2ca781
davluzius <davluzius@gmail.com>
2015-02-28 20:22:34 -0800

    Instructions - show/hide , add link to view multi-user behavior

--- client/views/Instructions.js
+++ client/views/Instructions.js
@@ -1,5 +1,15 @@
 Template.Instructions.events({
   'click .restoreDefaults' : function(e){
     Meteor.call('restoreDefaults');
+  },
+  'click #toggle-directions' : function(e){
+    e.preventDefault();
+    var el = $('.instructions-container');
+    el.toggle();
+    if(el.css("display")==='none' ){
+      $(e.target).html('show directions');
+    }else{
+      $(e.target).html('hide directions');
+    }
   }
 });
