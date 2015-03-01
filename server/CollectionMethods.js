fe4a7d8ba582ccd9c317e983aa85256632659208
davluzius <davluzius@gmail.com>
2015-03-01 13:34:12 -0800

    methods - separate Default Template methods from general Template methods

--- server/CollectionMethods.js
+++ server/CollectionMethods.js
@@ -2,44 +2,13 @@ var MAX_CHARS = 800;
 
 Meteor.startup(function(){
 
-
-  Meteor.call('setDefaults');
-
-  // restore the defaults every hour
-  var hour = 1000 * 60 * 60;
-  Meteor.setInterval(function(){
-    Meteor.call('restoreDefaults');
-  },hour); 
+  Meteor.call('createDefaultTemplate');
 
 });
 
 Meteor.methods({
 
-  saveHTML : function(newHTML,templateId,userId){
-    if( newHTML.length<MAX_CHARS && !!userId){
-      TemplateCollection.update({_id:templateId},{$set:{html:newHTML,lastModifiedBy:userId}},function(err,res){
-        if(err||res===0){
-          console.log("saveHTML err",err, res);
-        }else{
-          console.log("saveHTML OK!", res);
-        }
-      });
-    }
-  },
-
-  saveCSS : function(newCSS,templateId,userId){
-    if(newCSS.length<MAX_CHARS && !!userId){
-      TemplateCollection.update({_id:templateId},{$set:{css:newCSS,lastModifiedBy:userId}},function(err,res){
-        if(err||res===0){
-          console.log("saveCSS error",err, res);
-        }else{
-          console.log("saveCSS OK!",res);
-        }
-      });
-    }
-  },
-
-  setDefaults : function(){
+  createDefaultTemplate : function(){
 
     TemplateCollection.remove({});
     
@@ -54,8 +23,8 @@ Meteor.methods({
       }
     }
   },
-  // TODO lookup by name
-  restoreDefaults : function(){
+
+  restoreDefaultTemplate : function(){
 
     defaultTemplate.modified = new Date();
 
