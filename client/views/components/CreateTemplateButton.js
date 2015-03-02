a0cc56587bd3c4dac3f8e4ef20e5d804f2a3cc32
davluzius <davluzius@gmail.com>
2015-03-01 17:45:57 -0800

    move CreateTemplateButton into subdir

---
+++ client/views/components/CreateTemplateButton.js
@@ -0,0 +1,35 @@
+Template.CreateTemplateButton.rendered = function(){
+
+  $('#new-template-controls').hide();
+
+}
+
+Template.CreateTemplateButton.events({
+
+  'click #create-new-template-btn' : function(e){
+    $('#create-new-template-btn').hide();
+    $('#new-template-controls').show();
+  },
+
+  'click #confirm-new-template-btn' : function(e){
+    var name = $('#template-name-field').val();
+    if(name.length<2 || name.length>100){
+      $('#new-template-error').html('Must be between 2 and 100 chars.');
+      return;
+    }
+    Meteor.call('CreateNewTemplate',name,function(err,res){
+      if(err || res === 0){
+        console.warn("new template error! ", err,res );
+      }else{
+        Router.go('/'+res);  // navigate to the new template
+      }
+
+    }); 
+  },
+  'click #cancel-new-template-btn' : function(e){
+    $('#create-new-template-btn').show();
+    $('#new-template-controls').hide();
+    $('#template-name-field').val('');
+  },
+
+});
