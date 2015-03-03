83f6b29e0e1def7cabba34806839307756053be1
davluzius <davluzius@gmail.com>
2015-03-02 23:21:47 -0800

    Add Rename Template button

---
+++ client/views/components/RenameTemplateButton.js
@@ -0,0 +1,43 @@
+Template.RenameTemplateButton.rendered = function(){
+
+  $('#rename-template-controls').hide();
+
+}
+
+Template.RenameTemplateButton.events({
+
+  'click #rename-template-btn' : function(e){
+    $('#rename-template-btn').hide();
+    $('#rename-template-controls').show();
+    $('#template-name-field').val('');
+    $('#rename-template-field').focus();
+    $('#rename-template-error').html('');
+  },
+
+  'mouseup #confirm-rename-template-btn' : function(e){
+
+    var name = $('#rename-template-field').val();
+    if(name.length<2 || name.length>20){
+      $('#rename-template-error').html('Must be between 2 and 20 chars.');
+      return;
+    }
+    var userId = Session.get('userId');
+    Meteor.call('RenameTemplate',name,this._id,userId,function(err,res){
+      if(err || res === 0){
+        console.warn("new template error! ", err,res );
+        $('#rename-template-error').html(err);
+      }else{
+        $('#rename-template-btn').show();
+        $('#rename-template-controls').hide();
+        $('#rename-template-field').val('');
+      }
+
+    }); 
+  },
+  'click #cancel-rename-template-btn' : function(e){
+    $('#rename-template-btn').show();
+    $('#rename-template-controls').hide();
+    $('#rename-template-field').val('');
+  },
+
+});
