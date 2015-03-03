d883a671e946b3944b41930a2247cf35fdaec683
davluzius <davluzius@gmail.com>
2015-03-03 01:01:00 -0800

    Tabs nav for Codemirror files

---
+++ client/views/template-editor/Tabs.js
@@ -0,0 +1,23 @@
+
+Template.Tabs.rendered = function(){
+
+  $(document).ready(function() {
+    $(".tabs-menu a").click(function(event) {
+      event.preventDefault();
+      $(this).parent().addClass("current");
+      $(this).parent().siblings().removeClass("current");
+      var tab = $(this).attr("href");
+      $(".tab-content").not(tab).css("display", "none");
+      $(tab).show();
+    });
+
+
+    // give code mirror time to initialize itself
+    setTimeout(function(){
+      $('#tab-2').hide();
+      $('#tab-3').hide();
+    },20);
+
+  });
+
+}
