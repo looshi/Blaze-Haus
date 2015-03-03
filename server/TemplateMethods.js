befa9b560f83368b51f5c010b1952a7c00a6c660
davluzius <davluzius@gmail.com>
2015-03-02 20:12:43 -0800

    remove redundant validation, since simple schema will catch this

--- server/TemplateMethods.js
+++ server/TemplateMethods.js
@@ -1,7 +1,6 @@
 Fiber = Npm.require('fibers');
 Future = Npm.require('fibers/future');
 
-var MAX_CHARS = 800;
 
 
 Meteor.methods({
@@ -69,39 +68,39 @@ Meteor.methods({
   },
 
   saveHTML : function(newHTML,templateId,userId){
-    if( newHTML.length<MAX_CHARS && !!userId){
-      TemplateCollection.update({_id:templateId},{$set:{html:newHTML,lastModifiedBy:userId}},function(err,res){
-        if(err||res===0){
-          console.log("saveHTML err",err, res);
-        }else{
-          //console.log("saveHTML OK!", res);
-        }
-      });
-    }
+
+    TemplateCollection.update({_id:templateId},{$set:{html:newHTML,lastModifiedBy:userId}},function(err,res){
+      if(err||res===0){
+        console.log("saveHTML err",err, res);
+      }else{
+        //console.log("saveHTML OK!", res);
+      }
+    });
+    
   },
 
   saveJS : function(newJS,templateId,userId){
-    if( newJS.length<MAX_CHARS && !!userId){
-      TemplateCollection.update({_id:templateId},{$set:{js:newJS,lastModifiedBy:userId}},function(err,res){
-        if(err||res===0){
-          console.log("saveJS err",err, res);
-        }else{
-          //console.log("saveHTML OK!", res);
-        }
-      });
-    }
+
+    TemplateCollection.update({_id:templateId},{$set:{js:newJS,lastModifiedBy:userId}},function(err,res){
+      if(err||res===0){
+        console.log("saveJS err",err, res);
+      }else{
+        //console.log("saveHTML OK!", res);
+      }
+    });
+    
   },
 
   saveCSS : function(newCSS,templateId,userId){
-    if(newCSS.length<MAX_CHARS && !!userId){
-      TemplateCollection.update({_id:templateId},{$set:{css:newCSS,lastModifiedBy:userId}},function(err,res){
-        if(err||res===0){
-          console.log("saveCSS error",err, res);
-        }else{
-          //console.log("saveCSS OK!",res);
-        }
-      });
-    }
+
+    TemplateCollection.update({_id:templateId},{$set:{css:newCSS,lastModifiedBy:userId}},function(err,res){
+      if(err||res===0){
+        console.log("saveCSS error",err, res);
+      }else{
+        //console.log("saveCSS OK!",res);
+      }
+    });
+    
   },
 
 
