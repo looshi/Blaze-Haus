2a2b75b1f7a9de3d2472fdb302b0f97833ef72c8
looshi <>
2015-03-16 21:55:57 -0700

    fix visual jump in Editor upon render

--- client/views/template-editor/Tabs.js
+++ client/views/template-editor/Tabs.js
@@ -4,6 +4,10 @@ Tabs which appear above each css, js, and html file.
 */
 Template.Tabs.rendered = function(){
 
+  $('#tab-1').css('opacity','0'); // hides a visual jump as codemirror resizes
+  $('#tab-2').css('opacity','0');
+  $('#tab-3').css('opacity','0');
+
   $(document).ready(function() {
     $(".tabs-menu a").click(function(event) {
       event.preventDefault();
@@ -19,6 +23,11 @@ Template.Tabs.rendered = function(){
     setTimeout(function(){
       $('#tab-2').hide();
       $('#tab-3').hide();
+
+      $('#tab-1').css('opacity','1');
+      $('#tab-2').css('opacity','1');
+      $('#tab-3').css('opacity','1');
+
     },20);
 
   });
