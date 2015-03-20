2ea41b67aef92d3c878eb0718268d98ad03aa33b
looshi <>
2015-03-20 15:58:23 -0700

    fix multi-user editing ... again

--- client/views/template-editor/TextEditor.js
+++ client/views/template-editor/TextEditor.js
@@ -20,36 +20,54 @@ TextEditor = function(_textArea,_type,_id) {
   this.editor = CodeMirror.fromTextArea(textArea,options);
   this.editor.display.wrapper.id = _id;
 
+  this.autoSave = true;
+
 }
 
+
+/**
+* setValue
+* will set the value, but won't cause a "change" in the debounce handler
+* this is done so that changes coming from a different user can be set without calling Save functions
+* @param {String} _text
+
+*/
 TextEditor.prototype.setValue = function(_text){
+  this.autoSave = false;
   this.editor.getDoc().setValue(_text);
 }
 
- 
+
+
 /**
 * debounce
-* wraps event handler in a debounce, calls callback with params
+* wraps event handler in a debounce, calls callback with params, used for Auto Save behavior of the editor
 * @param {String} _eventName, "change" is the only one we use right now, but there are many available
 * @param {Function} _handler , callback
 * @param {String} _templateId , _id of document from TemplateCollection
 * @param {String} _userId, which user made the change
 */
 TextEditor.prototype.debounce = function(_eventName,_handler,_templateId,_userId){
-  
-  var self = this;
 
+  var self = this;
   this.editor.getDoc().on(_eventName, 
 
     _.debounce(  
       function(e){
-
-        _handler(e.getValue(),_templateId,_userId,self);
+        console.log( " Autosave ? " , self.autoSave );
+        if(self.autoSave){
+          _handler(e.getValue(),_templateId,_userId);
+        }else{
+          self.autoSave = true;
+        }
+       
          
       }, 300 )  // no semicolon
     
   );
 }
+
+
 
 /**
 * on
@@ -65,4 +83,7 @@ TextEditor.prototype.on = function(_eventName,_handler,_codeType,_self){
   });
 }
   
-
+TextEditor.prototype.off = function(_eventName,_handler){
+  this.editor.getDoc().off(_eventName,_handler);
+}
+  
