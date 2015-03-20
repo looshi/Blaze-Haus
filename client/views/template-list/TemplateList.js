e2ec16b5b9e30bf2f61d908b137bb7fcc65ce57d
davluzius <davluzius@gmail.com>
2015-03-19 19:31:04 -0700

    add User Profile and links to profile pages

--- client/views/template-list/TemplateList.js
+++ client/views/template-list/TemplateList.js
@@ -11,19 +11,12 @@ Template.TemplateList.helpers({
   getNumTemplates : function(){
     return TemplateCollection.find().count();
   },
-  getOwner : function(){
-
-    if(this.owner!=='anonymous'&&this.owner!=='System'){
-      
-      var user = Meteor.users.findOne(this.owner);
-      if(user && user.services){
-        var id = user.services.github.id;
-        var html = "<a href='/"+id+"'><img src='https://avatars.githubusercontent.com/u/"+id+"?s=24'/></a>"
-        return html;
-      }
-    }else{
-      return '&nbsp;&nbsp;';
+  getLikes : function(){
+    console.log("likes " , this.likes );
+    if(this.likes>0){
+      return "&#9829;  "+this.likes;
     }
+     
   }
 
 })
