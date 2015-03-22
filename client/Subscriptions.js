de69d13540c8f640dc9dedd1637bdc50732c821e
davluzius <davluzius@gmail.com>
2015-03-22 16:32:52 -0700

    remove unused publications,  about page updates

--- client/Subscriptions.js
+++ client/Subscriptions.js
@@ -1,11 +1,5 @@
 Tracker.autorun(function () {
   // the data that populates the list of Templates on the homepage
   Meteor.subscribe("summaryTemplateData"); 
-
-  // the data that Templates can retrieve via helpers 
-  // These subscriptions will be managed individually 
-  // once we allow data import https://github.com/looshi/Meteor-Live-Template-Editor/issues/7
-  Meteor.subscribe("addressData");
-  Meteor.subscribe("peopleData") ;
   Meteor.subscribe("userData");
 });
