0cf14f25f10565906438a4fc7a840599a808267e
looshi <>
2015-03-23 19:52:51 -0700

    fix styles

--- client/views/template-list/TemplateList.js
+++ client/views/template-list/TemplateList.js
@@ -6,10 +6,10 @@ Lists all Templates in the database with summary information.
 Template.TemplateList.helpers({
 
   getTopRated : function(){
-    return TemplateCollection.find({},{sort:{likes:-1}});
+    return TemplateCollection.find({},{sort:{likes:-1},limit:1000});
   },
   getLatest : function(){
-    return TemplateCollection.find({},{sort:{created:-1}});
+    return TemplateCollection.find({},{sort:{created:-1},limit:1000});
   },
   getNumTemplates : function(){
     return TemplateCollection.find().count();
