88d462e693b9c90c257fdcecf1df53266e8293fa
looshi <>
2015-03-23 20:50:38 -0700

    add Published button

--- client/views/template-list/TemplateList.js
+++ client/views/template-list/TemplateList.js
@@ -6,10 +6,10 @@ Lists all Templates in the database with summary information.
 Template.TemplateList.helpers({
 
   getTopRated : function(){
-    return TemplateCollection.find({owner:{$ne:'anonymous'}},{sort:{likes:-1},limit:1000});
+    return TemplateCollection.find({owner:{$ne:'anonymous'},published:true},{sort:{likes:-1},limit:1000});
   },
   getLatest : function(){
-    return TemplateCollection.find({owner:{$ne:'anonymous'}},{sort:{created:-1},limit:1000});
+    return TemplateCollection.find({owner:{$ne:'anonymous'},published:true},{sort:{created:-1},limit:1000});
   },
   getNumTemplates : function(){
     return TemplateCollection.find().count();
